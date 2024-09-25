c2a7ff76d4d35f1d5e92357ba6f889c5cb196a88
Farhan <farhan.harits05@gmail.com>
2024-09-25 21:27:43 +0700

    Mini Project Final

--- JS/index.js
+++ JS/index.js
@@ -1,14 +1,15 @@
 // mengganti atau mengisi kata sambutan pada page pertama
 function replaceName() {
   let name = prompt("What's Your Name?", "");
+  console.log(name);
   document.getElementById("name").innerHTML = name
-} 
+}
 replaceName()
 
 
 // animasi scroll agar terlihat lebih smooth
 function buttonAbout() {
-  document.getElementById("target-section-our-profile").scrollIntoView({ behavior: "smooth"});
+  document.getElementById("target-section-about").scrollIntoView({ behavior: "smooth" });
 } // button about & button hit me
 function buttonProject() {
   document.getElementById("target-section-project").scrollIntoView({ behavior: "smooth" });
@@ -28,23 +29,25 @@ function plusDivs(n) {
 function showDivs(n) {
   var i;
   var x = document.getElementsByClassName("img-slideshow");
-  if (n > x.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = x.length};
+  if (n > x.length) { slideIndex = 1 }
+  if (n < 1) { slideIndex = x.length };
   for (i = 0; i < x.length; i++) {
     x[i].style.display = "none";
   }
-  x[slideIndex-1].style.display = "block";
+  x[slideIndex - 1].style.display = "block";
 }
 
-document.getElementById('contactForm').onsubmit = function(event) {
+
+// validate form
+document.getElementById('contactForm').onsubmit = function (event) {
   event.preventDefault();
 
   var name = document.getElementById('nama').value;
   var email = document.getElementById('email').value;
   var message = document.getElementById('message').value;
 
-  if(name || email || message) {
-      alert(`Thank you for your message, here are your submitted details :\nName : ${name}\nEmail : ${email}\nMessage:  ${message}`);
-  } 
+  if (name || email || message) {
+    alert(`Thank you for your message, here are your submitted details :\nName : ${name}\nEmail : ${email}\nMessage:  ${message}`);
+  }
   this.submit();
 }; 
